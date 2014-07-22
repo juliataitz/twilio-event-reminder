1f99df6c6e6ebd1e8c5b4a8781f1c7e46c4e1177
juliataitz <juliataitz@gmail.com>
2014-07-22 13:00:15 -0400

    omniauth working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,14 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to root_url
+    redirect_to '/users/new'
   end
 
 
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url
+    redirect_to '/users/new'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   resources :users
   resource :verifications
 
-  root to: 'users#new'
+  root to: 'users#index'
 
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
-  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', to: redirect('/'), as: 'signout', via: [:get, :post]
 
 end

2fb2c57a839718e33c55fb68f572baf6e2d91c4e
Matthias Loker <mmloker@gmail.com>
2014-07-26 13:54:11 -0400

    refactors sending messages to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,34 +2,21 @@ class UsersController < ApplicationController
 
   def new
     @user = User.find_by(session[:user_id])
-    # @user = User.new
-    @user.facebook
-    #binding.pry
+    @user.message_content
   end
  
   def update
-    #raise params.inspect
-    # @user = User.new(user_params)
     @user = User.find_by(session[:user_id])
     @user.phone = user_params[:phone]
     @user.save
-    #binding.pry
     if @user.save
-      # notice "Thank you! You will receive an SMS shortly with verification instructions."
       render text: "Thank you! You will receive an SMS shortly with verification instructions."
-      # redirect_to '/users/new'
-      # create a link
-      # @facebook ||= Koala::Facebook::API.new(user_params[:oauth_token])
-      # binding.pry
-      
-      # Instantiate a Twilio client
       client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
       
-      # Create and send an SMS message
       client.account.sms.messages.create(
         from: TWILIO_CONFIG['from'],
         to: @user.phone,
-        body: "You once said: #{@user.facebook}"#"HEY #{@user.name}! YOU ARE AWESOME!"
+        body: "#{@user.message_content}"
       )
     else
       render :new
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,70 +17,24 @@ class User < ActiveRecord::Base
 
   end
 
-  # def facebook
-  #   # @user = User.find_by(session[:user_id])
-  #   # @user = User.find_by(:id)
-  #   @user = User.all.first
-  #   fb_statuses = []
-  #   fb_times = []
-  #   @facebook ||= Koala::Facebook::API.new(oauth_token)
-  #   #binding.pry
-  #   @user_profile = @facebook.get_object("me")
-  #   @statuses = @facebook.get_connections(@user_profile["id"], "statuses")
-  #   @statuses.each do |status|
-  #     fb_statuses << status["message"]
-  #     fb_times << status["updated_time"]
-  #     # @user.messages.each do |message|
-  #     #   message.content = status["message"]
-  #     # end
-  #     # binding.pry
-  #     # @user.build_message(:content => status["message"] )
-  #   end
-  #   # return today_status(fb_statuses, fb_times)
-  #   return today_status(fb_statuses)
-  #   # @user.save
-  # end
-    def facebook
-    # @user = User.find_by(session[:user_id])
-    # @user = User.find_by(:id)
-    @user = User.all.first
-    # fb_statuses = []
-    # fb_times = []
-    @facebook ||= Koala::Facebook::API.new(oauth_token)
-    #binding.pry
-    @user_profile = @facebook.get_object("me")
+  def message_content
+    @user_profile = get_user_profile
     @statuses = @facebook.get_connections(@user_profile["id"], "statuses")
-    binding.pry
-    @today_status = @statuses.raw_response["data"].choice
-    @today_status_message = @today_status['message']
-    return @today_status_message
-    # @statuses.each do |status|
-    #   fb_statuses << status["message"]
-    #   fb_times << status["updated_time"]
-    #   # @user.messages.each do |message|
-    #   #   message.content = status["message"]
-    #   # end
-    #   # binding.pry
-    #   # @user.build_message(:content => status["message"] )
-    # end
-    # # return today_status(fb_statuses, fb_times)
-    # return today_status(fb_statuses)
-    # @user.save
-
+    @todays_message = @statuses.sample
+    @content = @todays_message["message"]
+    @time = Date.parse(@todays_message['updated_time'])
+    if @content.length + 32 > 160
+      @content = "On #{@time}, you said: #{@content[0..120]}..."
+    else
+      @content = "On #{@time}, you said: #{@content}"
+    end
   end
 
-  # def today_status(fb_statuses)
-  #   index = Random.rand(1..fb_statuses.length)
-  #   @today_status = fb_statuses[index]
-  #   # @today_time = fb_times[index]
-  #   if @today_status.length + 15 > 160
-  #     @today_status = @today_status[0..140] + "..."
-  #   else
-  #     @today_status
-  #   end
-  #   # @today_time
-  #   # binding.pry
-  # end
+  private
 
+  def get_user_profile
+    @facebook ||= Koala::Facebook::API.new(oauth_token)
+    @facebook.get_object("me")
+  end
 
 end

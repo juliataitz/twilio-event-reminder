d3bfc85107d44b1ce801bb1a79bba0aac5c955db
juliataitz <juliataitz@gmail.com>
2014-07-27 19:14:00 -0400

    user controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,14 @@
 class SessionsController < ApplicationController
 
   def create
-    user = User.from_omniauth(env["omniauth.auth"])
-    session[:user_id] = user.id
-    redirect_to user_path(user)
+    @user = User.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to user_path(user)
+    redirect_to user_path(@user)
   end
 
 end

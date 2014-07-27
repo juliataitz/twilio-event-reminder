b5b6b21bcb68ec31b61b8f353e9155ed4a99e079
juliataitz <juliataitz@gmail.com>
2014-07-27 18:08:58 -0400

    sass

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   root to: 'users#index'
 
-  get 'auth/:provider', as: "facebook_login"
+  # get 'auth/:provider', as: "facebook_login"
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post], as: :auth
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   get 'signout', to: 'sessions#destroy', to: redirect('/'), as: 'signout', via: [:get, :post]

2daac1312d4d7f6d8204df5d464db908fcc267b1
juliataitz <juliataitz@gmail.com>
2014-07-28 18:54:35 -0400

    Add figaro

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,2 +1,5 @@
-path = File.join(Rails.root, "config/twilio.yml")
-TWILIO_CONFIG = YAML.load(File.read(path))[Rails.env] || {'sid' => '', 'from' => '', 'token' => ''}
+TWILIO_CONFIG = {
+  'sid' => ENV['TWILIO_SID'],
+  'from' => ENV['TWILIO_FROM'],
+  'token' => ENV['TWILIO_TOKEN']
+}
